770e986ed0b2c0ddd26026b27166c5bdab28b4df
Chris Herring <chris.herring.iphone@gmail.com>
2015-10-27 14:11:21 +1100

    Brought in the work that compares and diffs the yaml files

--- lib/i18ndiff.rb
+++ lib/i18ndiff.rb
@@ -1,5 +1,54 @@
 require "i18ndiff/version"
+require "yaml"
 
 module I18ndiff
-  # Your code goes here...
+
+  def self.convert(*args)
+    @old_file = args[0]
+    @new_file = args[1]
+    compare_files(old_file, new_file)
+  end
+
+  def self.compare_files(old_file, new_file)
+    old_yml_str = YAML.load_file(old_file)
+    new_yml_str = YAML.load_file(new_file)
+    new_hash = {}
+    compare_hash(new_yml_str, old_yml_str, new_hash)
+    new_hash.to_yaml
+  end
+
+  def self.compare_hash(new_yaml, old_yaml, new_hash, context = [])
+    new_yaml.each do |key, value|
+
+      unless old_yaml.key?(key)
+        hash = new_hash
+
+        context.each do |c|
+          if hash.has_key?(c)
+            hash = hash[c]
+          else
+            hash = hash.store(c, {})
+          end
+        end
+
+        hash = hash.store(key, value)
+        next
+      end
+
+      value2 = old_yaml[key]
+      if value.is_a?(Hash)
+        compare_hash(value, value2, new_hash, (context + [key]))
+        next
+      end
+    end
+  end
+
+  def self.old_file
+    @old_file
+  end
+
+  def self.new_file
+    @new_file
+  end
+
 end
--- spec/i18ndiff_spec.rb
+++ spec/i18ndiff_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
 describe I18ndiff do
-  it 'has a version number' do
-    expect(I18ndiff::VERSION).not_to be nil
+
+  let(:old_file) { File.expand_path('../fixtures/old_translations.yml', __FILE__) }
+  let(:new_file) { File.expand_path('../fixtures/new_translations.yml', __FILE__) }
+  let(:diff) { YAML.load_file(File.expand_path('../fixtures/diff.yml', __FILE__)).to_yaml }
+
+  it 'has an old file' do
+    I18ndiff.convert(old_file, new_file)
+    expect(I18ndiff.old_file).to eq(old_file)
   end
 
-  it 'has an old file'
+  it "has a new file to compare to the old file" do
+    I18ndiff.convert(old_file, new_file)
+    expect(I18ndiff.new_file).to eq(new_file)
+  end
 
-  it "has a new file to compare to the old file"
-
-  it "outputs the stuff in the new file that is not in the old file"
+  it "outputs the stuff in the new file that is not in the old file" do
+    expect(I18ndiff.convert(old_file, new_file)).to eq(diff)
+  end
 
 end
